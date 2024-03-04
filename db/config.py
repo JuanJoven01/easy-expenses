19e6a539d5eb4d48641b11f6f7ba180acd4fea70
JuanJoven01 <juanjov95@hotmail.com>
2024-03-04 06:54:33 -0500

    fst commit, starting with sqlalchemyconfig

---
+++ db/config.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from dotenv import dotenv_values, load_dotenv
+load_dotenv(override=False)
+environment = dotenv_values(".env")
+
+db_host=environment['POSTGRES_HOST']
+db_user=environment['POSTGRES_USER']
+db_password=environment['POSTGRES_PASSWORD']
+db_name=environment['DB_NAME']
+
+engine = create_engine(f'postgresql://{db_host}:{db_password@{db_host}/{db_name}}', echo=True)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
