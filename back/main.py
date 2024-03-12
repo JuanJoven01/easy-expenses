b1144f30d6caf644f0c3e95b340bbf86ccdbcbf8
JuanJoven01 <juanjov95@hotmail.com>
2024-03-12 17:47:19 -0500

    temporaly disabled cors and apikey

--- back/main.py
+++ back/main.py
@@ -16,18 +16,18 @@ from middlewares.api_key import check_api_key, host
 app = FastAPI()
 
 app.add_middleware(ErrorHandler)
-app.middleware(check_api_key)
-origins = [
-    "http://localhost:5173",
-    host
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.middleware(check_api_key)
+# origins = [
+#     "http://localhost:5173",
+#     host
+# ]
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(users_router)
 app.include_router(categories_router)
