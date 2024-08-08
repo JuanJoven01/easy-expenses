fd11cbb949bbae0491957a6f338f37cccc6308a8
JuanJoven01 <juanjov95@hotmail.com>
2024-08-08 17:19:17 +0200

    refactor(all): remove frontremove all the front and dedicate the repo to full back

--- front/src/components/Title/index.jsx
+++
@@ -1,8 +0,0 @@
-
-
-
-export const Title = (props) => {
-    return(
-        <h1 className=" font-satoshi-bolditalic text-center text-5xl bg-gradient-to-r from-blue-500 to-delta text-transparent bg-clip-text pb-2 pt-1">{props.text}</h1>
-    )
-}
